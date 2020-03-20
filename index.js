4642ec0490e71f15ed38250ea27d4388f89b4270
jing-xuan <tayjxn@gmail.com>
2020-03-20 16:55:03 +0800

    overview page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const https = require("https");
 const partials = require("express-partials");
 const app = express();
 app.set('view engine', 'ejs');
@@ -10,7 +11,19 @@ const port = 80;
 app.get('/', function(req, res){
   var data = {
     overall: 0.6,
-    breakdown: [30, 20, 50] //positive, neutral, negative
+    breakdown: [30, 20, 50], //positive, neutral, negative
+    healthcare: {
+      overall: 0.7,
+      breakdown: [50, 30, 20]
+    },
+    finance: {
+      overall: 0.3,
+      breakdown: [10, 30, 60]
+    },
+    education: {
+      overall: 0.5,
+      breakdown: [30, 10, 60]
+    }
   }
   res.render("\index", {
     data: data
